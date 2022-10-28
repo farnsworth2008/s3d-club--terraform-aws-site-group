cb981c3436403e490219608bd60546ff05500741
Mark Farnsworth <farnsworth2008@gmail.com>
2022-10-28 18:30:41 +0000

    0.1.32-s3d-1052 Changed
    - Added `s3d-flow-json` in `changes` module

--- main.tf
+++ main.tf
@@ -70,7 +70,7 @@ module "name" {
 # tfsec:ignore:aws-ec2-no-public-ingress-sgr
 module "sg_ingress_open" {
   count  = var.cidrs == null ? 0 : 1
-  source = "github.com/s3d-club/terraform-aws-sg_ingress_open?ref=0.1.10-s3d-1011"
+  source = "github.com/s3d-club/terraform-aws-sg_ingress_open?ref=0.1.10-s3d-1012"
 
   cidr        = var.cidrs
   cidr6       = var.cidr6s
