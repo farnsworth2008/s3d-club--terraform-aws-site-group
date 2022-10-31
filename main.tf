ed57d1d5fc506d3d468f06f4ea672e82b69b663c
Mark Farnsworth <farnsworth2008@gmail.com>
2022-10-31 21:44:15 +0000

    0.1.35-s3d-1007 Changed
    - Added `aws_s3_bucket_server_side_encryption_configuration` resource
    - Sorted code

--- main.tf
+++ main.tf
@@ -49,7 +49,7 @@ module "ec2_work" {
 
 module "ecr" {
   for_each = toset(var.ecrs)
-  source   = "github.com/s3d-club/terraform-aws-ecr?ref=0.1.16-s3d-1001"
+  source   = "github.com/s3d-club/terraform-aws-ecr?ref=0.1.16-s3d-1002"
 
   kms_key_arn = local.kms_key_arn
   name_prefix = each.key
@@ -57,7 +57,7 @@ module "ecr" {
 }
 
 module "name" {
-  source = "github.com/s3d-club/terraform-external-name?ref=0.1.15-s3d-1002"
+  source = "github.com/s3d-club/terraform-external-name?ref=v0.1.15"
 
   context = var.name
   path    = path.module
