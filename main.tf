8c849e9a7110ca880cd463c7e9d491f9186b7756
Mark Farnsworth <farnsworth2008@gmail.com>
2022-11-03 03:24:44 +0000

    0.1.39-s3d-1001 Changed
    - Updated `ec2_work` module

--- main.tf
+++ main.tf
@@ -49,7 +49,7 @@ module "ec2_work" {
 
 module "ecr" {
   for_each = toset(var.ecrs)
-  source   = "github.com/s3d-club/terraform-aws-ecr?ref=v0.1.16"
+  source   = "github.com/s3d-club/terraform-aws-ecr?ref=v0.1.17"
 
   kms_key_arn = local.kms_key_arn
   name_prefix = each.key
@@ -57,7 +57,7 @@ module "ecr" {
 }
 
 module "name" {
-  source = "github.com/s3d-club/terraform-external-name?ref=v0.1.15"
+  source = "github.com/s3d-club/terraform-external-name?ref=v0.1.16"
 
   context = var.name
   path    = path.module
